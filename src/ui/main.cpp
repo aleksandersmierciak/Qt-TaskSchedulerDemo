6cfed8ea607ac0b5506af44ef639f892c6d0a82c
Aleksander Śmierciak <aleksander.smierciak@gmail.com>
2014-01-18 22:36:45 +0100

    Set a context property on TaskScheduler object

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -1,10 +1,14 @@
 #include "qtquick2controlsapplicationviewer.h"
+#include "taskscheduler.h"
 
 int main(int argc, char *argv[])
 {
     Application app(argc, argv);
 
+    TaskScheduler taskScheduler(&app);
+
     QtQuick2ControlsApplicationViewer viewer;
+    viewer.rootContext()->setContextProperty("taskScheduler", &taskScheduler);
     viewer.setMainQmlFile(QStringLiteral("qml/ui/main.qml"));
     viewer.show();
 
