29d7380032c468c990501670cdc9724597d6065d
Aleksander Śmierciak <aleksander.smierciak@gmail.com>
2014-01-18 19:23:39 +0100

    Implement TaskScheduler based on McNaughton algorithm

--- src/scheduling/taskscheduler.cpp
+++ src/scheduling/taskscheduler.cpp
@@ -1,6 +1,87 @@
+#include <stdexcept>
+#include <QDebug>
+#include <QListIterator>
 #include "taskscheduler.h"
 
 
 TaskScheduler::TaskScheduler()
 {
+}
+
+void TaskScheduler::schedule(int machineCount, QList<QSharedPointer<Job> > jobs)
+{
+    if (jobs.count() == 0)
+    {
+        throw std::invalid_argument("One or more jobs need to be provided");
+    }
+    if (m_machineCount == 0)
+    {
+        throw std::invalid_argument("One or more machines need to be provided");
+    }
+
+    m_jobs = jobs;
+    m_machineCount = machineCount;
+
+    calculateMaxTime();
+    initializeMachines();
+    assignJobs();
+}
+
+void TaskScheduler::initializeMachines()
+{
+    clear();
+    for (unsigned int i = 0; i < m_machineCount; ++i)
+    {
+        m_machines.append(QSharedPointer<Machine>(new Machine(i, m_maxTime)));
+    }
+}
+
+void TaskScheduler::clear()
+{
+    m_machines.clear();
+}
+
+void TaskScheduler::calculateMaxTime()
+{
+    unsigned int maxTime = 0;
+    unsigned int totalDuration = 0;
+    for (auto job : m_jobs)
+    {
+        maxTime = std::max(maxTime, job.data()->duration());
+        totalDuration += job.data()->duration();
+    }
+    totalDuration /= m_machineCount;
+
+    m_maxTime = std::max(maxTime, totalDuration);
+}
+
+void TaskScheduler::assignJobs()
+{
+    QListIterator<QSharedPointer<Machine> > machineIterator(m_machines);
+    QListIterator<QSharedPointer<Job> > jobIterator(m_jobs);
+    while (jobIterator.hasNext())
+    {
+        Machine &machine = *machineIterator.peekNext();
+        QSharedPointer<Job> job = jobIterator.peekNext();
+        if (!machine.tryAdd(job))
+        {
+            unsigned int durationLeft = machine.durationLeft();
+            if (durationLeft > 0)
+            {
+                QSharedPointer<Job> splitJob = job.data()->split(durationLeft);
+                machine.add(job);
+                machineIterator.next();
+                machineIterator.peekNext().data()->add(splitJob);
+                jobIterator.next();
+            }
+            else
+            {
+                machineIterator.next();
+            }
+        }
+        else
+        {
+            jobIterator.next();
+        }
+    }
 }
--- src/scheduling/taskscheduler.h
+++ src/scheduling/taskscheduler.h
@@ -1,13 +1,28 @@
 #ifndef TASKSCHEDULER_H
 #define TASKSCHEDULER_H
 
+#include <QList>
+#include <QSharedPointer>
 #include "scheduling_global.h"
+#include "machine.h"
 
 class SCHEDULINGSHARED_EXPORT TaskScheduler
 {
-
 public:
     TaskScheduler();
+
+    void schedule(int machineCount, QList<QSharedPointer<Job> > jobs);
+
+private:
+    unsigned int m_maxTime;
+    unsigned int m_machineCount;
+    QList<QSharedPointer<Machine> > m_machines;
+    QList<QSharedPointer<Job> > m_jobs;
+
+    void initializeMachines();
+    void clear();
+    void calculateMaxTime();
+    void assignJobs();
 };
 
 #endif // TASKSCHEDULER_H
